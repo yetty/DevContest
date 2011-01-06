2e0de7374deaa8f68acdf2f8af55e60e171f4f0b
Juda Kaleta <juda.kaleta@gmail.com>
2011-01-06 21:24:51 +0100

    Hidding outputs

--- devcontest/model/db/runner.py
+++ devcontest/model/db/runner.py
@@ -81,8 +81,6 @@ class Runner(object):
 	    return str
 
 	def exe(self, file, fileIn=None, time_limit=10, memory_limit=10*1024, i=None):
-		print time_limit
-		print file
 		err = ""
 		ret = ""
 		status = False
